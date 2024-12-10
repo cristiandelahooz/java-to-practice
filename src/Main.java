0324381e3bd69f17509dbbc1247fc1c9b63e1de2
Cristian de la Hoz <cyrondelahoz@gmail.com>
2024-12-10 11:21:23 -0400

    doc: delete documentation

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Please enter a number: ");
